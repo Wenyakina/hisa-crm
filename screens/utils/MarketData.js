c247a8379944146150db49be209cb7d1fab4e439
Simeon Obwogo <simeon.obwogo79@gmail.com>
2025-08-06 21:14:12 +0300

    mpesa wallet topup

--- screens/utils/MarketData.js
+++ screens/utils/MarketData.js
@@ -4,7 +4,7 @@ export const previousMarketData = [
     { id: 3, category: 'Gainer', ticker: 'Faida', price: 27.85, change: 9.22},
     { id: 4, category: 'Gainer', ticker: 'SIB', price: 0.31, change: 6.9 },
     { id: 5, category: 'Gainer', ticker: 'Genghis', price: 65.0, change: 3.59 },
-    { id: 6, category: 'Loser', ticker: 'NCBA', price: 240.5, change: -9.93 },
+    { id: 6, category: 'Loser', ticker: 'NCBA', price: 67.5, change: -9.93 },
     { id: 7, category: 'Loser', ticker: 'Equity', price: 45.75, change: -9.85 },
     { id: 8, category: 'Loser', ticker: 'KCB', price: 7.64, change: -9.69 },
     { id: 9, category: 'Loser', ticker: 'ABSA', price: 1.74, change: -3.33 },
